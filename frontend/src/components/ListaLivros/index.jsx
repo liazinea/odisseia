247c6e689b961404dfb2afff100b3ae5f98171ea
Liazinea <feitosalivia23@gmail.com>
2024-11-28 16:52:11 -0300

    Renomeia componente TabelaLivros para ListaLivros

--- frontend/src/components/ListaLivros/index.jsx
+++ frontend/src/components/ListaLivros/index.jsx
@@ -2,16 +2,10 @@ import styles from './index.module.scss';
 import { MdOutlineEdit } from "react-icons/md";
 import { IoMdTrash } from "react-icons/io";
 
-const TabelaLivros = ({data, capa, titulo, numRegistro}) => {
+const ListaLivros = ({data, capa, titulo, numRegistro}) => {
   return (
     <div className={styles.principal}>
-      <div className={styles.head}>
-        <div className={styles.data}>Data de Cadastro</div>
-        <div className={styles.capa}>Capa</div>
-        <div className={styles.titulo}>Título</div>
-        <div className={styles.num}>Número de Registro</div>
-        <div className={styles.opcoes}>Opções</div>
-      </div>
+      
       <div className={styles.body}>
         <div className={styles.data}>{data}</div>
         <div className={styles.capa}><img src={capa} className={styles.imagem}/></div>
@@ -26,4 +20,4 @@ const TabelaLivros = ({data, capa, titulo, numRegistro}) => {
   )
 }
 
-export default TabelaLivros
+export default ListaLivros
