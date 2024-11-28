7b7e007d4ae9928fb47e1c099108afc607a55cc3
Liazinea <feitosalivia23@gmail.com>
2024-11-28 16:24:57 -0300

    Adiciona componente TabelaLivros

--- frontend/src/components/TabelaLivros/index.jsx
+++ frontend/src/components/TabelaLivros/index.jsx
@@ -1,28 +1,28 @@
 import styles from './index.module.scss';
+import { MdOutlineEdit } from "react-icons/md";
+import { IoMdTrash } from "react-icons/io";
 
 const TabelaLivros = ({data, capa, titulo, numRegistro}) => {
   return (
-    <>
-    <table className={styles.tabela}>
-        <tr className={styles.head}>
-            <th>Data de Cadastro</th>
-            <th>Capa</th>
-            <th>Título</th>
-            <th>Número de Resgistro</th>
-            <th>Opções</th>
-        </tr>
-        <tr>
-            <td>{data}</td>
-            <td><img src={capa} alt="" /></td>
-            <td>{titulo}</td>
-            <td>{numRegistro}</td>
-            <td>
-                <div>botao1</div>
-                <div>botao2</div>
-            </td>
-        </tr>
-    </table>
-    </>
+    <div className={styles.principal}>
+      <div className={styles.head}>
+        <div className={styles.data}>Data de Cadastro</div>
+        <div className={styles.capa}>Capa</div>
+        <div className={styles.titulo}>Título</div>
+        <div className={styles.num}>Número de Registro</div>
+        <div className={styles.opcoes}>Opções</div>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.data}>{data}</div>
+        <div className={styles.capa}><img src={capa} className={styles.imagem}/></div>
+        <div className={styles.titulo}>{titulo}</div>
+        <div className={styles.num}>{numRegistro}</div>
+        <div className={styles.opcoes}>
+          <MdOutlineEdit size={30} />
+          <IoMdTrash size={30} color='#C00F0C'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
