734ccdb1637bb1edc4d47beec527031f14d0ec0f
gabrielapl <gabriela_pl10@hotmail.com>
2024-11-28 16:10:08 -0300

    Acrescenta media query de TextoIcon

--- frontend/src/components/TextoIcon/index.jsx
+++ frontend/src/components/TextoIcon/index.jsx
@@ -1,13 +1,12 @@
 import styles from "./index.module.scss";
 import React from 'react';
-import { IoBookOutline } from "react-icons/io5";
 
 
 const TextoIcon = ({nome, icon, descricao}) => {
   return (
     <div className={styles.geral}>
       <p className= {styles.titulo}>{nome}</p>
-      <IoBookOutline className={styles.icon}/>
+      <div className={styles.icon}>{icon}</div>
       <p className= {styles.info}>{descricao}</p>
     </div>
   )
