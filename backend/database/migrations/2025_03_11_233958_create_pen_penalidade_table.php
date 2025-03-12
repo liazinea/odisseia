9618065d37f7f565e9da1579ea0334727123fd04
Patrick0402 <bruno.patrick0402@gmail.com>
2025-03-11 21:46:03 -0300

    feat:making new migrations

---
+++ backend/database/migrations/2025_03_11_233958_create_pen_penalidade_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('pen_penalidade', function (Blueprint $table) {
+            $table->id('pen_id');
+            $table->date('pen_data');
+            $table->timestamps();
+
+            $table->foreign('usu_id')->references('usu_id')->on('usu_usuario');
+            $table->foreign('emp_id')->references('emp_id')->on('emp_emprestimo');
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('pen_penalidade');
+    }
+};
