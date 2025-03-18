0e3cb69fd5028a92e4cd30109d4a8ad3dd5f0a97
Liazinea <feitosalivia23@gmail.com>
2025-03-18 13:30:55 -0300

    Divididno componentes da tela de Login

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import styles from './index.module.scss'
+import CardForm from '../../components/Cards/CardForm'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className={styles.principal}>
+      <div className={styles.logo}>
+        <img src="/forma-login.svg" alt="" className={styles.logoFundo}/>
+        <div className={styles.logoTexto}>
+          <div className={styles.centraliza}>
+            <img src="/logo-odisseia.svg" alt="" className={styles.logoImagem}/>
+          </div>
+          <h2 className={styles.logoTitulo}>Odisseia</h2>
+          <p className={styles.logoSubtitulo}>E.E Ernesto Quissak</p>
+        </div>
+      </div>
+      <div className={styles.form}>
+        <CardForm/>
+      </div>
+    </div>
   )
 }
 
