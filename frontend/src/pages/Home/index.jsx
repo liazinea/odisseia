7f768c29166b99ecf37d2bda231a6deb7443cc78
Patrick0402 <bruno.patrick0402@gmail.com>
2025-04-01 20:42:42 -0300

    feat: header e home page

---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../../components/layout/Navbar"
+import HeaderHome from "../../components/layout/HeaderHome"
+import Footer from "../../components/layout/Footer";
+
+const Home = () => {
+  return (
+    <>
+        <Navbar/>
+        <HeaderHome/>
+        <Footer />
+    </>
+  );
+};
+
+export default Home;
