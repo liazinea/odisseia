6e430919e9faea6ce3934036040861f0a7f77d8e
Liazinea <feitosalivia23@gmail.com>
2025-04-13 00:44:14 -0300

    Arruma footer do layout das páginas

--- frontend/src/layouts/PageLayout/index.jsx
+++ frontend/src/layouts/PageLayout/index.jsx
@@ -3,19 +3,18 @@ import styles from "./index.module.scss";
 import Navbar from "../../components/layout/Navbar";
 import Footer from "../../components/layout/Footer";
 
-const PageLayout = () =>{
+const PageLayout = () => {
+  document.body.classList.add("background-main");
 
-    document.body.classList.add('background-main');
+  return (
+    <div className={styles.principal}>
+      <Navbar />
+      <main role="main" className={`${styles.main}`}>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
-    return(
-        <>
-        <Navbar/>
-            <main role="main" className={`${styles.main}`}>
-                <Outlet/>
-            </main>
-            <Footer />
-        </>
-    );
-}
-
-export default PageLayout
+export default PageLayout;
