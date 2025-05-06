c258b6c3e080cca2fd7ee166d859940b9bf18f51
Patrick0402 <bruno.patrick0402@gmail.com>
2025-05-06 20:10:29 -0300

    fix: home adm

--- frontend/src/components/layout/SessaoAdmHome/index.jsx
+++ frontend/src/components/layout/SessaoAdmHome/index.jsx
@@ -9,42 +9,48 @@ const SessaoAdmHome = () => {
     <div className={styles.container}>
       <BotaoAdmHome
         type="button"
-        nomeBotao="usuarios"
-        icon="/users-icon.svg"
-        texto="Usuários"
-        onClick={()=>navigate('/usuarios')}
-        />
+        nomeBotao="realizar-emprestimo"
+        icon="/plus-icon.svg"
+        texto="Realizar empréstimo"
+      />
       <BotaoAdmHome
+        type="button"
+        nomeBotao="consultar-emprestimos"
+        icon="/registros-icon.svg"
+        texto="Consultar empréstimos"
+      />
+        <BotaoAdmHome
         type="button"
         nomeBotao="livros"
         icon="/livro-icon.svg"
         texto="Livros"
         onClick={()=>navigate('/opcoes-livro')}
-        />
+      />
       <BotaoAdmHome
         type="button"
-        nomeBotao="editoras"
-        icon="/editoras-icon.svg"
-        texto="editoras"
-        />
+        nomeBotao="usuarios"
+        icon="/users-icon.svg"
+        texto="Usuários"
+        onClick={()=>navigate('/usuarios')}
+      />
       <BotaoAdmHome
         type="button"
         nomeBotao="autores"
         icon="/autores-icon.svg"
         texto="Autores"
-        />
+      />
       <BotaoAdmHome
         type="button"
         nomeBotao="generos"
         icon="/generos-icon.svg"
         texto="Gêneros"
         onClick={()=>navigate('/generos')}
-        />
+      />
       <BotaoAdmHome
         type="button"
-        nomeBotao="registros"
-        icon="/registros-icon.svg"
-        texto="Registros"
+        nomeBotao="editoras"
+        icon="/editoras-icon.svg"
+        texto="editoras"
       />
     </div>
   );
