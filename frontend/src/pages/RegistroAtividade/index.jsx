2fe3f5ed466396c2aacd83d5174a30ece9f56751
Liazinea <feitosalivia23@gmail.com>
2025-05-13 16:55:38 -0300

    Cria estilo da Tabela de Registros

--- frontend/src/pages/RegistroAtividade/index.jsx
+++ frontend/src/pages/RegistroAtividade/index.jsx
@@ -1,9 +1,9 @@
 import styles from "./index.module.scss";
+import HeaderPagina from '../../components/layout/HeaderPagina'
 import { useAuth } from "../../context/AuthContext";
 import { useEffect } from "react";
-import {
-  useNavigate
-} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import TabelaRegistros from "../../components/Tabelas/TabelaRegistros";
 
 const RegistroAtividade = () => {
   const { token } = useAuth();
@@ -16,7 +16,11 @@ const RegistroAtividade = () => {
   }, [token]);
   return (
     <>
-    
+        <HeaderPagina titulo={'Registro de Atividades'} />
+        <TabelaRegistros emprestimos={[{id: 1, created_at: '05-11-2026', numEmprestimo: 24, titulo: "O Espreitador", isbn: "29845151485111", status: 'Reservado'}, 
+            {id: 2, created_at: '05-11-2026', numEmprestimo: 24, titulo: "O Espreitador", isbn: "29845151485111", status: 'Reservado'}, 
+            {id: 3, created_at: '05-11-2026', numEmprestimo: 24, titulo: "O Espreitador", isbn: "29845151485111", status: 'Reservado'}, 
+            {id: 4, created_at: '05-11-2026', numEmprestimo: 24, titulo: "O Espreitador", isbn: "29845151485111", status: 'Reservado'}]}/>
     </>
   );
 };
