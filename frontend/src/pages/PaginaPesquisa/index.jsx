ec50eed26d627c8598e9d359ad8a89270af9602d
Luiza <luizadirias@gmail.com>
2025-06-07 18:08:50 -0300

    adiciona pagina de pesquisa

--- frontend/src/pages/PaginaPesquisa/index.jsx
+++ frontend/src/pages/PaginaPesquisa/index.jsx
@@ -1,9 +1,54 @@
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import useLivros from '../../hooks/useLivros'; // ajuste o caminho se necessário
+import CardLivrosMaisEmprestados from '../../components/Cards/CardLivrosMaisEmprestados';
+import styles from './index.module.scss';
+import HeaderHome from '../../components/layout/HeaderHome';
 
 const PaginaPesquisa = () => {
   const { termo } = useParams();
+  const { buscaLivros } = useLivros();
+  const [resultados, setResultados] = useState([]);
+  const [carregando, setCarregando] = useState(true);
 
-  return <h1>Termo da pesquisa: {termo}</h1>;
-}
+  useEffect(() => {
+    const fetchLivros = async () => {
+      setCarregando(true);
+      const todosLivros = await buscaLivros();
+      const termoNormalizado = termo.toLowerCase();
+
+      const filtrados = todosLivros.filter((livro) =>
+        livro.titulo.toLowerCase().includes(termoNormalizado)
+      );
+
+      setResultados(filtrados);
+      setCarregando(false);
+    };
+
+    fetchLivros();
+  }, [termo, buscaLivros]);
+
+  return (
+    <div>
+<HeaderHome/>
+      <h3>Resultados para: "{termo}"</h3>
+      {carregando ? (
+        <p>Carregando...</p>
+      ) : resultados.length > 0 ? (
+        <div className={styles.container}>
+
+          {resultados.map((livro) => (
+            <div className={styles.cards}>
+            <CardLivrosMaisEmprestados key={livro.id} livro={livro} />
+            </div>
+          ))}
+        </div>
+        
+      ) : (
+        <p>Nenhum livro encontrado.</p>
+      )}
+    </div>
+  );
+};
 
 export default PaginaPesquisa;
