e637dd97b8b339fd342f20eceaf479bd070b9d23
Patrick0402 <bruno.patrick0402@gmail.com>
2025-06-13 23:01:30 -0300

    fix: melhorias nos seeders existentes

--- backend/database/seeders/AutorSeeder.php
+++ backend/database/seeders/AutorSeeder.php
@@ -13,7 +13,34 @@ class AutorSeeder extends Seeder
      */
     public function run(): void
     {
-       Autor::create(['aut_nome'=>'Clarice Lispector']);
-       Autor::create(['aut_nome'=>'Maurício de Sousa']);
+        Autor::create(['aut_nome' => 'Clarice Lispector']);
+        Autor::create(['aut_nome' => 'Maurício de Sousa']);
+        Autor::create(['aut_nome' => 'Machado de Assis']);
+        Autor::create(['aut_nome' => 'Cecília Meireles']);
+        Autor::create(['aut_nome' => 'Carlos Drummond de Andrade']);
+        Autor::create(['aut_nome' => 'Monteiro Lobato']);
+        Autor::create(['aut_nome' => 'Franz Kafka']);
+        Autor::create(['aut_nome' => 'Jorge Amado']);
+        Autor::create(['aut_nome' => 'José Saramago']);
+        Autor::create(['aut_nome' => 'Fernando Pessoa']);
+        Autor::create(['aut_nome' => 'J.K. Rowling']);
+        Autor::create(['aut_nome' => 'Graciliano Ramos']);
+        Autor::create(['aut_nome' => 'Guimarães Rosa']);
+        Autor::create(['aut_nome' => 'Vinícius de Moraes']);
+        Autor::create(['aut_nome' => 'Ariano Suassuna']);
+        Autor::create(['aut_nome' => 'Carolina Maria de Jesus']);
+        Autor::create(['aut_nome' => 'Érico Veríssimo']);
+        Autor::create(['aut_nome' => 'William Shakespeare']);
+        Autor::create(['aut_nome' => 'Fiodor Dostoiévski']);
+        Autor::create(['aut_nome' => 'Liev Tolstói']);
+        Autor::create(['aut_nome' => 'Edgar Allan Poe']);
+        Autor::create(['aut_nome' => 'Isaac Asimov']);
+        Autor::create(['aut_nome' => 'Charles Bukowski']);
+        Autor::create(['aut_nome' => 'Simone de Beauvoir']);
+        Autor::create(['aut_nome' => 'Albert Camus']);
+        Autor::create(['aut_nome' => 'Friedrich Nietzsche']);
+        Autor::create(['aut_nome' => 'Jean-Paul Sartre']);
+        Autor::create(['aut_nome' => 'Mark Twain']);
+        Autor::create(['aut_nome' => 'Oscar Wilde']);
     }
 }
--- backend/database/seeders/GeneroSeeder.php
+++ backend/database/seeders/GeneroSeeder.php
@@ -13,5 +13,25 @@ class GeneroSeeder extends Seeder
     {
         Genero::create(['gen_nome' => 'Suspense']);
         Genero::create(['gen_nome' => 'Drama']);
+        Genero::create(['gen_nome' => 'Romance']);
+        Genero::create(['gen_nome' => 'Ficção Científica']);
+        Genero::create(['gen_nome' => 'Fantasia']);
+        Genero::create(['gen_nome' => 'Terror']);
+        Genero::create(['gen_nome' => 'Aventura']);
+        Genero::create(['gen_nome' => 'Poesia']);
+        Genero::create(['gen_nome' => 'Biografia']);
+        Genero::create(['gen_nome' => 'Autoajuda']);
+        Genero::create(['gen_nome' => 'Ensaios']);
+        Genero::create(['gen_nome' => 'História']);
+        Genero::create(['gen_nome' => 'Policial']);
+        Genero::create(['gen_nome' => 'Crônica']);
+        Genero::create(['gen_nome' => 'Infantil']);
+        Genero::create(['gen_nome' => 'Humor']);
+        Genero::create(['gen_nome' => 'Filosofia']);
+        Genero::create(['gen_nome' => 'Clássicos']);
+        Genero::create(['gen_nome' => 'Conto']);
+        Genero::create(['gen_nome' => 'Distopia']);
+        Genero::create(['gen_nome' => 'Psicologia']);
+        Genero::create(['gen_nome' => 'História em Quadrinhos']);
     }
 }
