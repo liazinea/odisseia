462da48e1134293ca9b3f47a8ad9754e179aaafb
Liazinea <feitosalivia23@gmail.com>
2025-06-30 16:17:18 -0300

    Adiciona frontend na pagina de validar código

--- frontend/src/pages/ValidarCodigo/index.jsx
+++ frontend/src/pages/ValidarCodigo/index.jsx
@@ -1,11 +1,18 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import api from "../../services/api";
+import InputForm from "../../components/Inputs/InputForm";
+import BotaoFormLogin from "../../components/Botao/BotaoFormLogin";
+import ModalMensagem from "../../components/Modal/ModalMensagem";
+import styles from "./index.module.scss";
+import { useState } from "react";
 
 const ValidarCodigo = () => {
   const location = useLocation();
   const navigate = useNavigate();
-
+  const [mensagemModal, setMensagemModal] = useState();
+  const [sucessoModal, setSucessoModal] = useState(false);
+  const [modalMensagemAberto, setModalMensagemAberto] = useState();
   const email = location.state?.email || "";
 
   const {
@@ -14,22 +21,64 @@ const ValidarCodigo = () => {
     formState: { errors },
   } = useForm();
 
+  const fecharModalMensagem = () => {
+    setModalMensagemAberto(false);
+    setMensagemModal("");
+    if(sucessoModal){
+      navigate("/");
+      setSucessoModal(false)
+    }
+  };
+
   const onSubmit = async (data) => {
     try {
-      await api.post("/confirmar-codigo", data);
-      alert("Senha definida com sucesso!");
-      navigate("/"); // ou onde você quiser redirecionar após sucesso
+      const response = await api.post("/confirmar-codigo", data);
+      console.log(data)
+      setMensagemModal("Senha definida com sucesso!");
+      setMensagemModal(response.data.message);
+      setModalMensagemAberto(true);
+      console.log(modalMensagemAberto)
+      setSucessoModal(true)
     } catch (error) {
       console.log(error);
-      alert("Erro ao validar código.");
+      setMensagemModal(error.response.data.message);
+      setModalMensagemAberto(true);
     }
   };
 
   return (
-    <div style={{ maxWidth: "400px", margin: "50px auto" }}>
-      <h2>Validar Código</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div style={{ marginBottom: "10px" }}>
+    <div className={styles.principal}>
+      <div className={styles.logo}>
+        <div className={styles.logoFundo}></div>
+        <div className={styles.logoTexto}>
+          <div className={styles.centraliza}>
+            <img
+              src="/logo-odisseia.svg"
+              alt=""
+              className={styles.logoImagem}
+            />
+          </div>
+          <div>
+            <h2 className={styles.logoTitulo}>Odisseia</h2>
+            <p className={styles.logoSubtitulo}>E.E Ernesto Quissak</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.form}>
+        <div className={styles.cardFundo}>
+          <h2 className={styles.cardTitulo}>Validar Código</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <InputForm
+              type={"email"}
+              nomeCampo={"email"}
+              placeholder={"Digite o email"}
+              required={true}
+              filledInput={true}
+              {...register("email", { required: "Email obrigatório" })}
+              defaultValue={email}
+              error={errors}
+            />
+            {/* <div style={{ marginBottom: "10px" }}>
           <label>Email</label>
           <input
             type="email"
@@ -37,28 +86,62 @@ const ValidarCodigo = () => {
             defaultValue={email}
           />
           {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
-        </div>
+        </div> */}
+            <InputForm
+              type={"number"}
+              nomeCampo={"codigo"}
+              placeholder={"Código recebido"}
+              required={true}
+              {...register("code", { required: "Código obrigatório" })}
+              error={errors}
+            />
 
-        <div style={{ marginBottom: "10px" }}>
+            {/* <div style={{ marginBottom: "10px" }}>
           <label>Código recebido</label>
           <input
             type="text"
             {...register("code", { required: "Código obrigatório" })}
           />
           {errors.code && <p style={{ color: "red" }}>{errors.code.message}</p>}
-        </div>
+        </div> */}
 
-        <div style={{ marginBottom: "10px" }}>
+            <InputForm
+              type={"password"}
+              nomeCampo={"senha"}
+              placeholder={"Nova senha"}
+              required={true}
+              {...register("password", { required: "Senha obrigatória" })}
+              error={errors}
+            />
+
+            {/* <div style={{ marginBottom: "10px" }}>
           <label>Nova senha</label>
           <input
             type="password"
             {...register("password", { required: "Senha obrigatória" })}
           />
-          {errors.password && <p style={{ color: "red" }}>{errors.password.message}</p>}
-        </div>
+          {errors.password && (
+            <p style={{ color: "red" }}>{errors.password.message}</p>
+          )}
+        </div> */}
 
-        <button type="submit">Salvar senha</button>
-      </form>
+            {/* <button type="submit">Salvar senha</button> */}
+            <div className={styles.botao}>
+              <BotaoFormLogin
+                type={"button"}
+                nomeBotao={"submit"}
+                texto={"Redefinir senha"}
+                onClick={handleSubmit(onSubmit)}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+      <ModalMensagem
+        mensagemModal={mensagemModal}
+        closeModal={fecharModalMensagem}
+        modalAberto={modalMensagemAberto}
+      />
     </div>
   );
 };
